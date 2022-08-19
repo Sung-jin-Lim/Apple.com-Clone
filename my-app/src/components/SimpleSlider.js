57e0d4aab57eeb1df3c4497f4f6fad8465e6a541
Sungjin Lim <85204388+Sung-jin-Lim@users.noreply.github.com>
2022-08-19 16:24:27 +0900

    full size works now

--- my-app/src/components/SimpleSlider.js
+++ my-app/src/components/SimpleSlider.js
@@ -16,7 +16,7 @@ export default function SimpleSlider() {
     pauseOnFocus: false,
     centerMode: true,
     // variableWidth: true,
-    centerPadding: "20%",
+    centerPadding: "10%",
 
     adaptiveHeight: true,
   };
@@ -26,50 +26,42 @@ export default function SimpleSlider() {
       <div>
         <img
           className="w-[1250px] "
-          src="https://is2-ssl.mzstatic.com/image/thumb/GT66HoV8VJL1IZwaqos_TQ/689x387.jpg"
+          src="https://is2-ssl.mzstatic.com/image/thumb/GT66HoV8VJL1IZwaqos_TQ/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is2-ssl.mzstatic.com/image/thumb/78-I7VenST4ztZYfwMf6AQ/689x387.jpg"
+          src="https://is2-ssl.mzstatic.com/image/thumb/78-I7VenST4ztZYfwMf6AQ/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is1-ssl.mzstatic.com/image/thumb/IyvoDdhWEjtSADaSryepIw/689x387.jpg"
+          src="https://is1-ssl.mzstatic.com/image/thumb/IyvoDdhWEjtSADaSryepIw/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is1-ssl.mzstatic.com/image/thumb/vPnF3gIE_xEMoXPI1SyluA/689x387.jpg"
+          src="https://is1-ssl.mzstatic.com/image/thumb/vPnF3gIE_xEMoXPI1SyluA/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is3-ssl.mzstatic.com/image/thumb/AWDRdQz0nepFpnsUNiTDuw/689x387.jpg"
+          src="https://is3-ssl.mzstatic.com/image/thumb/AWDRdQz0nepFpnsUNiTDuw/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is1-ssl.mzstatic.com/image/thumb/kjrFHClZ3Bt-pT0MJnwdFw/689x387.jpg"
-          alt=""
-        />
-      </div>
-      <div>
-        <img
-          className="w-[1250px]"
-          src="
-      https://is2-ssl.mzstatic.com/image/thumb/_dJgnWG8W30_qMRZYCEx-Q/689x387.jpg"
+          src="https://is1-ssl.mzstatic.com/image/thumb/kjrFHClZ3Bt-pT0MJnwdFw/1250x703.jpg"
           alt=""
         />
       </div>
@@ -77,7 +69,7 @@ export default function SimpleSlider() {
         <img
           className="w-[1250px]"
           src="
-      https://is5-ssl.mzstatic.com/image/thumb/ewq0-5ZSairVdRtcg43s0w/689x387.jpg"
+      https://is2-ssl.mzstatic.com/image/thumb/_dJgnWG8W30_qMRZYCEx-Q/1250x703.jpg"
           alt=""
         />
       </div>
@@ -85,14 +77,22 @@ export default function SimpleSlider() {
         <img
           className="w-[1250px]"
           src="
-        https://is2-ssl.mzstatic.com/image/thumb/WTDZpzkQlaatpb1X4w9jZg/689x387.jpg"
+      https://is5-ssl.mzstatic.com/image/thumb/ewq0-5ZSairVdRtcg43s0w/1250x703.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           className="w-[1250px]"
-          src="https://is5-ssl.mzstatic.com/image/thumb/P-1f-E_Qew_9syoQd34Wlw/689x387.jpg"
+          src="
+        https://is2-ssl.mzstatic.com/image/thumb/WTDZpzkQlaatpb1X4w9jZg/1250x703.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="w-[1250px]"
+          src="https://is5-ssl.mzstatic.com/image/thumb/P-1f-E_Qew_9syoQd34Wlw/1250x703.jpg"
           alt=""
         />
       </div>
